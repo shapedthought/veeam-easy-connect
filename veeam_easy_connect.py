a4c1f0d3749d17ba7198f8a3ca5c14f9ad92512b
Edward Howard <edward.x.howard@veeam.com>
2022-02-04 09:58:47 +0000

    Added a check on save file name

--- veeam_easy_connect.py
+++ veeam_easy_connect.py
@@ -65,6 +65,9 @@ class VeeamEasyConnect:
 
 
     def save_data(self, auth_type: str, file_name: str) -> None:
+        # Added a check in case extension was included
+        file_name = file_name.split(".")[0] if "." in file_name else file_name
+
         if auth_type == "basic":
             data = self.res_json_basic
         elif auth_type == "oauth":
