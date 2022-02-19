0aa953842feb6aee6be52eef60cf7551bdcddc17
Edward Howard <edward.x.howard@veeam.com>
2022-02-19 21:11:44 +0000

    Removed print response

--- src/veeam_easy_connect/__main__.py
+++ src/veeam_easy_connect/__main__.py
@@ -11,6 +11,8 @@ import sys
 Veeam Easy Connect
 This module does all the set up to allow you to connect to Veeam APIs
 """
+
+
 class VeeamEasyConnect:
     def __init__(self, username, password, verify=True) -> None:
         self.username = username
@@ -19,22 +21,22 @@ class VeeamEasyConnect:
         self.oauth_headers = {
             "accept": "application/json",
             "Content-type": "application/x-www-form-urlencoded"
-            }
+        }
         self.__get_settings()
         self.basic = False
         self.api_type = ""
-    
+
     def __em_login(self, address: str) -> None:
         self.basic = True
         self.basic_headers = {
-             "accept": "application/json",
+            "accept": "application/json",
         }
 
         self.basic_url = f"https://{address}:9398/api/sessionMngr/?v=latest"
         self.b_auth = HTTPBasicAuth(self.username, self.password)
-        self.response = requests.post(self.basic_url, headers=self.basic_headers, auth=self.b_auth, verify=self.verify)
+        self.response = requests.post(
+            self.basic_url, headers=self.basic_headers, auth=self.b_auth, verify=self.verify)
         self.response.raise_for_status()
-        print("OK")
         self.basic_id = self.response.headers["X-RestSvcSessionId"]
         self.res_json_basic = self.response.json()
         self.reqest_header = self.get_request_header()
@@ -43,36 +45,35 @@ class VeeamEasyConnect:
         if self.basic:
             self.__em_login(address)
         else:
-        # set up each login url endpoint and api version
+            # set up each login url endpoint and api version
             self.address = address
             self.oauth_url = f"https://{self.address}{self.url_end}"
-            self.oauth_data = {
-                "grant_type" : "password", 
-                "username" : self.username,
-                "password": self.password
-                }
-            self.response = requests.post(self.oauth_url, data=self.oauth_data, headers=self.oauth_headers, verify=self.verify)
+            self.oauth_data = {"grant_type": "password",
+                               "username": self.username,
+                               "password": self.password}
+            self.response = requests.post(
+                self.oauth_url, data=self.oauth_data, headers=self.oauth_headers, verify=self.verify)
             self.response.raise_for_status()
-            print("OK")
             self.res_json_oauth = self.response.json()
-            if not "mfa_token" in self.res_json_oauth:
+            if "mfa_token" not in self.res_json_oauth:
                 self.reqest_header = self.get_request_header()
             else:
-                print(f"MFA Token in response - use 'mfa_token_login' to with access code to continue")
+                print(
+                    f"MFA Token in response - use 'mfa_token_login' to with access code to continue")
 
     def mfa_token_login(self, code: str) -> None:
         self.token = self.res_json_oauth['mfa_token']
         self.mfa_url = f"https://{self.address}{self.url_end}"
         self.mfa_data = {
             "grant_type": "Mfa",
-            "mfa_token":  self.token,
+            "mfa_token": self.token,
             "mfa_code": code
         }
         # I don't think I need to change the Content-Type on this request- not clear
 
-        self.response = requests.post(self.mfa_url, data=self.mfa_data, headers=self.oauth_headers, verify=self.verify)
+        self.response = requests.post(
+            self.mfa_url, data=self.mfa_data, headers=self.oauth_headers, verify=self.verify)
         self.response.raise_for_status()
-        print("OK")
         self.res_json_oauth = self.response.json()
         self.reqest_header = self.get_request_header()
 
@@ -99,7 +100,7 @@ class VeeamEasyConnect:
     def get_access_token(self) -> str:
         if self.basic:
             return self.basic_id
-        else: 
+        else:
             return self.res_json_oauth['access_token']
 
     def get_request_header(self) -> dict:
@@ -116,9 +117,10 @@ class VeeamEasyConnect:
             bearer_string = 'Bearer ' + self.res_json_oauth['access_token']
             headers['Authorization'] = bearer_string
         return headers
-    
+
     def save_request_header(self, file_name: str) -> None:
-        file_name = file_name.split(".")[0] + ".json" if "." in file_name else file_name + ".json"
+        file_name = file_name.split(
+            ".")[0] + ".json" if "." in file_name else file_name + ".json"
         headers = self.get_request_header()
         with open(file_name, "w") as headers_file:
             json.dump(headers, headers_file)
@@ -169,6 +171,7 @@ class VeeamEasyConnect:
         self.api_type = "ent_man"
         return self
     # Update the URL and headers as needed
+
     def update_settings(self, api_type: str) -> None:
         if api_type == "o365":
             self.url_end = self.api_settings['o365']['url']
@@ -190,21 +193,21 @@ class VeeamEasyConnect:
             return
 
     def get(self, url: str) -> dict:
-        resp = requests.get(url, headers=self.reqest_header, verify=self.verify)
+        resp = requests.get(
+            url, headers=self.reqest_header, verify=self.verify)
         resp.raise_for_status()
-        print("OK")
         return resp.json()
 
     def post(self, url: str, data: dict) -> dict:
-        resp = requests.post(url, headers=self.reqest_header, data=data, verify=self.verify)
+        resp = requests.post(url, headers=self.reqest_header,
+                             data=data, verify=self.verify)
         resp.raise_for_status()
-        print("OK")
         return resp.json()
 
     def put(self, url: str, data: dict) -> dict:
-        resp = requests.put(url, headers=self.reqest_header, data=data, verify=self.verify)
+        resp = requests.put(url, headers=self.reqest_header,
+                            data=data, verify=self.verify)
         resp.raise_for_status()
-        print("OK")
         return resp.json()
 
     def update_port(self, port_num: str) -> None:
@@ -229,12 +232,13 @@ class VeeamEasyConnect:
         # set up for first request
 
         # remove the /token from the url_end brought in from config file
-        sso_url = "/".join(self.url_end.split("/")[0:3]) + "/identityProvider/signOnUrl?userName="
+        sso_url = "/".join(self.url_end.split("/")
+                           [0:3]) + "/identityProvider/signOnUrl?userName="
 
         # create the full url
         url = f"https://{address}:{sso_url}{sso_username}"
         api_version = self.oauth_headers.get("x-api-version")
-        headers = { "x-api-version":  api_version }
+        headers = {"x-api-version": api_version}
 
         # send first request
         data = requests.get(url, headers=headers, verify=self.verify)
@@ -251,9 +255,9 @@ class VeeamEasyConnect:
 
         # second request set up
         data = {
-            "username" : self.username,
+            "username": self.username,
             "password": self.password
-            }
+        }
         headers['Content-Type'] = "x-www-form-urlencoded"
 
         # get the code from the response to add to the new url
@@ -261,24 +265,24 @@ class VeeamEasyConnect:
         saml_url = f"https://{sso_address}//adfs/ls/?SAMLRequest={saml_code}"
 
         # send third request
-  
-        saml_res = requests.post(saml_url, data=data, headers=headers, verify=self.verify)
+
+        saml_res = requests.post(
+            saml_url, data=data, headers=headers, verify=self.verify)
         saml_res.raise_for_status()
 
         # third request set up
         token = saml_res.json().get("value")
 
         login_url = f"https://{sso_address}:11005/api/v1/identityProvider/token"
- 
+
         headers.pop("Content-Type")
         token_data = {"SamlResponse": token}
 
         # send third request
-        self.respose = requests.post(login_url, headers=headers, data=token_data, verify=self.verify)
+        self.respose = requests.post(
+            login_url, headers=headers, data=token_data, verify=self.verify)
         self.response.raise_for_status()
 
         # if all has gone well, set the data as normal
         self.res_json_oauth = self.response.json()
         self.reqest_header = self.get_request_header()
-        
-
